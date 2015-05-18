02626c14fce1bc8180f798daa9540e1566de10b7
jonathan-sahar <jonanthan.sahar@gmail.com>
2015-05-18 19:43:23 +0300

    add functions to featureCalculationFunctions.py  add constants.py

---
+++ constants.py
@@ -0,0 +1,64 @@
+__author__ = 'Jonathan'
+accl = ["diffSecs",
+  "N.samples",
+  "x.mean",
+  "x.absolute.deviation",
+  "x.standard.deviation",
+  "x.max.deviation",
+  "x.PSD.1",
+  "x.PSD.3",
+  "x.PSD.6",
+  "x.PSD.10",
+  "y.mean",
+  "y.absolute.deviation",
+  "y.standard.deviation",
+  "y.max.deviation",
+  "y.PSD.1",
+  "y.PSD.3",
+  "y.PSD.6",
+  "y.PSD.10",
+  "z.mean",
+  "z.absolute.deviation",
+  "z.standard.deviation",
+  "z.max.deviation",
+  "z.PSD.1",
+  "z.PSD.3",
+  "z.PSD.6",
+  "z.PSD.10",
+  "time",
+
+"Is sick"]
+
+audio = ["diffSecs",
+  "absolute.deviation",
+  "standard.deviation",
+  "max.deviation",
+  "PSD.250",
+  "PSD.500",
+  "PSD.1000",
+  "PSD.2000",
+  "MFCC.1",
+  "MFCC.2",
+  "MFCC.3",
+  "MFCC.4",
+  "MFCC.5",
+  "MFCC.6",
+  "MFCC.7",
+  "MFCC.8",
+  "MFCC.9",
+  "MFCC.10",
+  "MFCC.11",
+  "MFCC.12",
+  "time",
+  "Is sick"]
+
+i = 0
+accl_fields = {}
+for s in accl:
+    accl_fields[s] = i
+    i += 1
+audio_fields = {}
+i=0
+for s in audio:
+    audio_fields[s] = i
+    i += 1
--- featureCalculationFunctions.py
+++ featureCalculationFunctions.py
@@ -2,12 +2,46 @@ __author__ = 'Jonathan'
 import numpy as np
 import scipy.stats as stats
 import csv
-
+from entropy import entropy_ci
+from constants import audio_fields, accl_fields
 LONG_TIME_WINDOW = 300
 SHORT_TIME_WINDOW = 5
+ENTROPY_RADIUS = 5
 
-def stats1(timeWindow, timeWindowLength, filePath):
-    func_pointers = [(stats.tmax, 'max'), (stats.tmin,'min' ), (stats.trim_mean,'mean'), (stats.m)] #TODO add more functions and tuple with feature name
+def _mode(values): return stats.mode(values)[0][0]
+def _range(a): return stats.tmax(a) - stats.tmin(a)
+def _entropy(a): return entropy_ci(a, ENTROPY_RADIUS)
+
+psd_D = {
+    "low": 1,
+    "mid": 3,
+    "high":6
+}
+def _cross_correlation_accl(window, energy_type):
+    '''
+    :param window:
+    :param energy_type: one of "low", "mid", "high"
+    :return: cross correlation between movement in X and Y
+    '''
+    psd = str(psd_D[energy_type])
+    a = window[:, accl_fields["x.PSD."+ psd]]
+    b = window[:, accl_fields["y.PSD."+ psd]]
+    return stats.pearsonr(a,b)[0]
+
+
+def statisticsForAllColoumns(timeWindow, timeWindowLength, filePath):
+    func_pointers = [(stats.tmax, 'max'),
+                     (stats.tmin,'min' ),
+                     (stats.trim_mean,'mean'),
+                     (_mode, 'mode'),
+                     (stats.tstd, 'std'),
+                     (np.nanmedian, 'median'),
+                     (stats.skew, 'skew'),
+                     (stats.kurtosis, 'kurtosis'),
+                     (_entropy, 'entropy'),
+                     (stats.kurtosis, 'kurtosis'),
+
+                                ] #TODO add more functions
 
     data = []
     reader = csv.reader(filePath)
@@ -18,9 +52,13 @@ def stats1(timeWindow, timeWindowLength, filePath):
         values = [func(column) for func in func_pointers]
     return values
 
-def statsForLongTimeWindow():
+
 
 def statsForLongTimeWindow():
+    return
+
+def statsForLongTimeWindow():
+    return
 
 def lowFreqsCounter(window, shortTimeWindowPath):
     #if isLongTimeWindow == 0:
