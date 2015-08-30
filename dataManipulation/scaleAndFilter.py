95ebeece92f8395e044394c0d0debb4df9ddbb08
jonathan-sahar <jonanthan.sahar@gmail.com>
2015-08-30 11:12:10 +0300

    add scaleAndFilter.py - a function to remove windows where there is too much variance.

--- dataManipulation/scaleAndFilter.py
+++ dataManipulation/scaleAndFilter.py
@@ -1,14 +1,38 @@
 from sklearn.preprocessing import StandardScaler
-from utils.utils import filter_fields_by_name
+from utils.utils import filter_fields_by_name, castStructuredArrayToRegular
 import re
-# todo: complete me.
-# this is meant to scale all the data coming out from feature creation (it will be scaled again inside the cross validation)
-# and to remove time windows where the variance of the accelerometer/compass/whatever is too high
+import numpy as np
 
-STDEV_THRESHOLD = 0.5
-def scaleAndFilter(X, y):
+STDEV_THRESHOLD_UPPER = 0.5
+STDEV_THRESHOLD_LOWER = 0.05
+def filterLinesOnRegex(X, y, r = re.compile(r'([xyz]+_standard_deviation_mean)')):
+    '''
+    Removes time windows where the variance of the accelerometer/compass/whatever is too high
+    currently looks at accelerometer std
+    :param X: structured array
+    :param y: labels
+    :return:
+    '''
     scaler = StandardScaler()
-    X = scaler.fit_transform(X)
     r = re.compile(r'([xyz]+_standard_deviation_mean)')
     fields = filter_fields_by_name(X.dtype.names, r)
-    mask = X[fields] > STDEV_THRESHOLD
+
+    # filteredX is a scaled copy of the relevant features only
+    filteredX = castStructuredArrayToRegular(X[fields])
+    filteredX = scaler.fit_transform(filteredX)
+
+    #mask is a 1D array with True for every line in filteredX where all values are within range
+    mask = np.array([(filteredX < STDEV_THRESHOLD_UPPER) & (filteredX > STDEV_THRESHOLD_LOWER)])
+    mask = np.squeeze(mask) #remove empty 3rd dimension
+    mask = np.array([np.any(row) for row in mask])
+
+    #indices are the indices (=line numbers) where mask is True
+
+    indices = np.where(mask)
+    return X[indices], y[indices]
+
+
+
+
+
+
