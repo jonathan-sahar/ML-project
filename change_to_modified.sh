9edc5034c114dd17d29cebdbc1d0b33a9209cf8c
Jonathan  Sahar jonathan.s@campus.technion.ac.il <jonathan.s@csl-pomela1.cs.technion.ac.il>
2015-12-05 11:06:50 +0200

    add scripts to switch sklearn versions (our cahnges <--> original)

---
+++ change_to_modified.sh
@@ -0,0 +1,1 @@
+mv ~/workspace/packages/lib/python2.7/site-packages/modified_sklearn ~/workspace/packages/lib/python2.7/site-packages/sklearn
---
+++ change_to_orig.sh
@@ -0,0 +1,1 @@
+mv ~/workspace/packages/lib/python2.7/site-packages/orig_sklearn ~/workspace/packages/lib/python2.7/site-packages/sklearn
