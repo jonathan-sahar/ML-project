0b56a35253080ed6d54ae52d48844350ab50b02e
Jonathan  Sahar jonathan.s@campus.technion.ac.il <jonathan.s@csl-pomela1.cs.technion.ac.il>
2015-12-15 14:42:47 +0200

    10%, 8 folds, all predictors

--- optimizationAndPrediction/predict.py
+++ optimizationAndPrediction/predict.py
@@ -225,8 +225,8 @@ def predictOnWindows(data, lables, names):
     #==============================================================================================
     #each result is a Dictionary with all learning Iterations (features, 'all')
     #==============================================================================================
-    # predictor_types = ['SVM', 'RF', 'logisticReg'] 
-    predictor_types = ['RF']
+    predictor_types = ['SVM', 'RF', 'logisticReg'] 
+    # predictor_types = ['RF']
     
     results = {}
     #predictors = dict()
@@ -294,7 +294,7 @@ def predict():
     # labels = linePerFiveMinutesLabels 
     # names = linePerFiveMinutesNames 
     
-    data, labels, names = getRandomSample(0.5)
+    data, labels, names = getRandomSample(10)
     names = [names] # ugly hack: tuneAndTrain (called from predictOnWindows
                     # expects names to be the [0] element of another list)
     
